c9960ed66c21fd98db33b17b917f61f572d78c7a
Sathish <sathish.s@jeanmartin.com>
2021-01-14 22:51:56 +0530

    Initial Commit
    Initial Project Setup, using react native cli.

---
+++ ios/Pods/Target Support Files/Pods-amigo-amigoTests/Pods-amigo-amigoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_amigo_amigoTests : NSObject
+@end
+@implementation PodsDummy_Pods_amigo_amigoTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-amigo-tvOSTests/Pods-amigo-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_amigo_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_amigo_tvOSTests
+@end
